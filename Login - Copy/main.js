3d00ba701577b3ef0a1b702ae890b5a502a8f609
Ralfs <zanispleve.roblox@gmail.com>
2022-08-08 20:20:55 +0300

    Added 1st code

---
+++ Login - Copy/main.js
@@ -0,0 +1,11 @@
+function(e) {
+    t(e.target).siblings(o).removeClass("w--redirected-focus")
+  }
+  
+  function(e) {
+    t(e.target).siblings(".w-checkbox-input").toggleClass("w--redirected-checked")
+  }
+  
+  function(e) {
+    t(e.target).siblings(o).addClass("w--redirected-focus")
+  }
