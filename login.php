0a873aa3443c39b759421bac482e8c55d47388e0
Ralfs <zanispleve.roblox@gmail.com>
2023-01-06 15:11:44 +0200

    New Update

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="shortcut icon" href="">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="screen">
+        <form action="">
+            <header>Login</header>
+            <input type="text" placeholder="Username" class="inp">
+            <input type="password" placeholder="Password" class="inp">
+            <a href="" class="link">Forgot Password</a>
+            <input type="submit" value="Login" class="btn">
+            <p class="text">Do not have account? <a href="" class="link">Signup!</a></p>
+        </form>
+    </div>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SignUp</title>
+    <link rel="shortcut icon" href="">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="screen">
+        <form action="">
+            <header>Signup</header>
+            <input type="email" placeholder="Email" class="inp">
+            <input type="text" placeholder="Username" class="inp">
+            <input type="password" placeholder="Password" class="inp">
+            <input type="password" placeholder="Confirm Password" class="inp">
+            <input type="submit" value="Signup" class="btn">
+            <p class="text">Have an account? <a href="" class="link">Login!</a></p>
+        </form>
+    </div>
+</body>
+</html>
